49001c3b38d852d21e6eacdec5c0e545e9a944a5
heorohuy <heorohuy@users.noreply.github.com>
2026-08-14 21:52:26 -0400

    removing fleet api
    didnt work

--- src/api/sicarisFleet.js
+++
@@ -1,9 +0,0 @@
-export async function getSicarisFleet() {
-  const response = await fetch("/api/fleet");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch Sicaris fleet");
-  }
-
-  return response.json();
-}
